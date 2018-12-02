fc65a51bdbd22fc880dd9964dedd9c78d4071e83
nakster <g00332403@gmit.ie>
2018-12-02 18:47:19 +0000

    pool sound effect

--- Pool/Assets/scripts/BallScript.cs
+++ Pool/Assets/scripts/BallScript.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class BallScript : MonoBehaviour {
     //The ball script
     // for github
-    public GameObject gamecom;
+    private GameObject gamecom;
     public bool hascollided;
+
+    public AudioSource hit;
+
+    private Transform player;
+
+
+    void Start(){
+        hit = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter2D(Collision2D coll)
     {
         if ((coll.gameObject.tag == "strip" || coll.gameObject.tag == "solid") && !hascollided)
         {
+            hit.Play();
             Debug.Log(coll.gameObject.name);
             hascollided = true;
+        }else{
+             hascollided = false;
         }
     }
 }
