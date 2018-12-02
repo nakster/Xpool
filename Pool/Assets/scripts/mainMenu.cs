5567e7f9fcccf5c24c2f821cc522a151359a4f62
nakster <g00332403@gmit.ie>
2018-12-02 13:56:29 +0000

    error fixed

--- Pool/Assets/scripts/mainMenu.cs
+++ Pool/Assets/scripts/mainMenu.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour {
-
+    // This here goes to the game 
 	public void playGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
+    //this would quit out of the game 
     public void Quit()
     {
         Application.Quit();
