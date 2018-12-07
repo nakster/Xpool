15018eb5c4579406c4d50ef99192c18c5745a71c
nakster <g00332403@gmit.ie>
2018-12-07 01:50:41 +0000

    exports

---
+++ Pool/Assets/Editor/ExportPackage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExportPackage : MonoBehaviour {
+
+	[MenuItem ("Export/MyExport")]
+    static void export()
+    {
+        AssetDatabase.ExportPackage (AssetDatabase.GetAllAssetPaths(),PlayerSettings.productName + ".unitypackage",ExportPackageOptions.Interactive | ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies | ExportPackageOptions.IncludeLibraryAssets);
+    }
+}
